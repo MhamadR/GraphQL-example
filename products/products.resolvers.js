2c7e963b80998c53ffe6b0204fd0feba3564106c
MhamadR <mhamadraslan@gmail.com>
2024-04-29 21:11:01 +0300

    Add resolver functions for products

---
+++ products/products.resolvers.js
@@ -0,0 +1,29 @@
+const {
+  getAllProducts,
+  getProductsByPrice,
+  getProduct,
+  addNewProduct,
+  addNewProductReview,
+} = require("./products.model");
+
+module.exports = {
+  Query: {
+    products: () => {
+      return getAllProducts();
+    },
+    productsByPrice: (_, args) => {
+      return getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+      return getProduct(args.id);
+    },
+  },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+      return addNewProductReview(args.id, args.rating, args.comment);
+    },
+  },
+};
